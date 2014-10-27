7c2ea48187f6f4141a6441e27d936aa6a7639605
George Pchelkin <p4el48@yandex.ru>
2014-10-27 20:47:15 +0300

    Env fix

--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 # This file is used by Rack-based servers to start the application.
 
-ENV['server_mode'] = '1' # For detecting if this is a server start, see initializers/informator_loader.rb
+ENV["SERVER_MODE"] = "1" # For detecting if this is a server start, see initializers/informator_loader.rb
 
 require ::File.expand_path('../config/environment', __FILE__)
 run Rails.application
--- config/initializers/informator_loader.rb
+++ config/initializers/informator_loader.rb
@@ -1,6 +1,6 @@
 # Load feeds only with server, not with console/rake/clockwork/dj
 
-# if defined?(Rails::Server) # Works only for WEBrick and Thin
-if ENV['server_mode'] # Works only for all Rack-based servers, set in config.ru
+# if defined?(Rails::Server) # Works only for WEBrick and Thin (that start with rails s)
+if ENV["SERVER_MODE"] # Works for all Rack-based servers, set in config.ru
   Feed.load_file
 end
