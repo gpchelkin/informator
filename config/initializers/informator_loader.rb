0fa881fbf701a0085e6a22ca6239da103bfc4c27
George Pchelkin <p4el48@yandex.ru>
2014-11-17 00:14:54 +0300

    Notices Load Fix.

--- config/initializers/informator_loader.rb
+++ config/initializers/informator_loader.rb
@@ -2,5 +2,5 @@
 # if defined?(Rails::Server) # Works only for WEBrick and Thin (that start with rails s)
 if ENV["SERVER_MODE"] # Works for all Rack-based servers, set in config.ru
   Feed.load_file unless Feed.exists? # Don't reload feeds if loaded
-  Notice.load_file
+  Notice.load_file unless Notice.exists?
 end
