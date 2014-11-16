d3f6a3667f07b3d7ce5440649962cbcf6657b101
George Pchelkin <p4el48@yandex.ru>
2014-11-16 09:04:09 +0300

    Don't reload feeds if loaded

--- config/initializers/informator_loader.rb
+++ config/initializers/informator_loader.rb
@@ -1,6 +1,6 @@
-# Load feeds only with server, not with console/rake/clockwork/dj
+# Load only with server, not with console/rake/clockwork/dj
 # if defined?(Rails::Server) # Works only for WEBrick and Thin (that start with rails s)
 if ENV["SERVER_MODE"] # Works for all Rack-based servers, set in config.ru
-  Feed.load_file
+  Feed.load_file unless Feed.exists? # Don't reload feeds if loaded
   Notice.load_file
 end
